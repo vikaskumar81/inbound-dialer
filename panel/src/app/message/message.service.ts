48e7f02f295f84d3a20b5af84f9995d2ca50d076
vikaskumar81 <vikas@gventure.net>
2018-03-22 01:39:00 +0530

    Messages DataService and Model is updated
    Messages DataService and Model is updated

--- panel/src/app/message/message.service.ts
+++ panel/src/app/message/message.service.ts
@@ -1,8 +1,49 @@
 import { Injectable } from '@angular/core';
+import { HttpClient }   from '@angular/common/http';
+import { Observable }   from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import { Message } from './model/model.class';
+import { Option } from '../shared/model/model.class';
+import { APIURL } from '../app.routing';
 
 @Injectable()
 export class MessageService {
 
-  constructor() { }
+  private appmod="message/";
+  private data:Message;
+
+  constructor(private http : HttpClient) { }
+
+  public set Data(v : Message) {
+    this.data = v;
+  }
+  
+  public get Data() : Message {
+    return this.data;
+  }
+
+  public getMessage(): Observable<Message[]> {
+    return this.http.get<Message[]>(APIURL+this.appmod);
+  }
+
+  saveMessage(data:Message)
+  {
+    this.http.post(APIURL, data);
+  }
+
+  public detailMessage(key:Number):Observable<Message>
+  {
+    return this.http.get<Message>(APIURL+this.appmod+key);
+  }
+
+  public updateMessage(data:Message, key:Number)
+  {
+    this.http.put(APIURL+this.appmod+key, data);
+  }
+
+  public deleteMessage(key:Number)
+  {
+    this.http.delete(APIURL+this.appmod+key);
+  }
 
 }
---
+++ panel/src/app/message/model/model.class.ts
@@ -0,0 +1,6 @@
+export interface Message
+{
+    id:number,
+    filename: string,
+    file: string
+}
