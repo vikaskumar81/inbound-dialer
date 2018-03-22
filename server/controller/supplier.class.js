e18f31cdb24df1abac8ec83cf6908523a6e07a19
Gita Maurya <gita.maurya@gventure.net>
2018-03-22 17:12:34 +0530

    Supplier testing is done and working well with postman

--- server/controller/supplier.class.js
+++ server/controller/supplier.class.js
@@ -1,4 +1,5 @@
 var mysql=require('../model/database.class');
+const fs = require('fs');
 var table="provider";
 exports.List=function(req, res, next){
     mysql.Open();
@@ -12,6 +13,8 @@ exports.List=function(req, res, next){
 
 exports.Detail=function(req, res){
     mysql.Open();
+	console.log("Hello We Are into detail function")
+	console.log(req.body);
     var filter={
       "id":req.params.id
     };
@@ -26,29 +29,39 @@ exports.Detail=function(req, res){
 
 exports.Update=function(req, res){
     mysql.Open();
-    var data= req.body.data;
-    var filter={
+	console.log("Hello We Are into Update function")
+    var data= req.body;    
+	var filter={
       "id":req.params.id
     };
-    mysql.Update(data, table, filter);
+    mysql.Update(data, table, filter, function(data) {
+		res.send(data);
+	});
     mysql.Close();
     return res;
 }
 
 exports.Delete=function(req, res){
+console.log("Hello We Are into Delete function")
     mysql.Open();
     var user={
         "id":req.params.id
     };
 
-    mysql.Remove(user, table);
+    mysql.Remove(user, table, function(data) {
+		res.send(data);
+	});
     mysql.Close();
+	return res;
 }
 
 exports.AddNew=function(req, res){
     mysql.Open();
-    var user=req.params.data;
-
-    mysql.AddNew(table, data);
+    var user=req.body;	
+	console.log("Hello We are in Add New row in table");
+	console.log(user);
+    mysql.AddNew(table, user, function(data) {
+		res.send(data);
+	});
     mysql.Close();
 }
