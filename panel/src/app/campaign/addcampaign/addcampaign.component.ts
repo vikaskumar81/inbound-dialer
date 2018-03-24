099b49a42c041cb080a37d9c93270ca6f81d4a08
vikaskumar81 <vikas@gventure.net>
2018-03-24 16:25:11 +0530

    update the add campaign form
    Add form is made with HTML5 version ready

--- panel/src/app/campaign/addcampaign/addcampaign.component.ts
+++ panel/src/app/campaign/addcampaign/addcampaign.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {FormControl, Validators} from '@angular/forms';
+import {MAT_MOMENT_DATE_FORMATS, MomentDateAdapter} from '@angular/material-moment-adapter';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
 import { CampaignService } from '../campaign.service';
 import { Option } from '../../shared/model/model.class';
 import { DataSource } from '@angular/cdk/table';
@@ -11,11 +13,18 @@ import { MatSelect } from '@angular/material';
 @Component({
   selector: 'app-addcampaign',
   templateUrl: './addcampaign.component.html',
-  styleUrls: ['./addcampaign.component.css']
+  styleUrls: ['./addcampaign.component.css'],
+  providers: [
+    // `MomentDateAdapter` and `MAT_MOMENT_DATE_FORMATS` can be automatically provided by importing
+    // `MatMomentDateModule` in your applications root module. We provide it at the component level
+    // here, due to limitations of our example generation script.
+    {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
+    {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS},
+  ]
 })
 
 export class AddcampaignComponent implements OnInit {
-  options: FormGroup;
+
   message : Option[];
   foundmessage: boolean;
   supplier : Option[];
@@ -23,13 +32,16 @@ export class AddcampaignComponent implements OnInit {
   hour =['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23'];
   minutes =[];
   supoption: MatSelect;
+  stDate: Date;
 
   campaigndata = new CampaignForm();
 
-  constructor(private cpdata: CampaignService, private fb: FormBuilder) { }
+  constructor(private cpdata: CampaignService, private fb: FormBuilder) {
+   }
 
   ngOnInit()
   {
+    this.stDate = new Date().getDate();
     this.cpdata.getSupplier().subscribe(
       data => {
         if(data.length>0)
