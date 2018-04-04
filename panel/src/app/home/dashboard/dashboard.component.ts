2436db65ed71b5b24ba34b79918f8cdea19af83a
vikaskumar81 <vikas@gventure.net>
2018-04-04 13:12:11 +0530

    DashBoard Update
    Updated widget in dashboard

--- panel/src/app/home/dashboard/dashboard.component.ts
+++ panel/src/app/home/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WidgetGroupComponent } from '../../shared/widget-group/widget-group.component';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  widgetdata =[{"icon":"account circle","data":205,"label":"Agents"},{"icon":"explore","data":1306,"label":"Calls"},
+  {"icon":"language","data":400,"label":"Minutes"},{"icon":"contact phone","data":30000,"label":"Leads"}];
+
   constructor() {}
 
   ngOnInit() { }
